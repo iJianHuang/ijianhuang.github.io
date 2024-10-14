70d3b4c3594e9c95301090b5f15463eb2ce20f60
Jian Huang <jianhuang16888@gmail.com>
2024-10-13 23:04:33 -0400

    KNN, add batch processing

--- HouseSales/KNN/KNN.js
+++ HouseSales/KNN/KNN.js
@@ -1,41 +1,28 @@
 class KNN 
 {
-    constructor(datasetUrl) 
+    constructor(features, label) 
     {
-        this.datasetUrl = datasetUrl;
-        this.datasetCSV = tf.data.csv(this.datasetUrl);
-
+        this.features = features;
+        this.label = label;
+        this.#buildFeaturesAndLabel();
     }
 
-    async buildFeaturesAndLabel(featureFields, labelField) {
-        this.mapppedDataset = this.datasetCSV.map(record => { 
-            const row = [];
-            featureFields.forEach(p => row.push(record[p]));
-            row.push(record[labelField]);
-            return row;
-        });
-        this.mapppedDatasetArray = await this.mapppedDataset.toArray();    
-        tf.util.shuffle(this.mapppedDatasetArray); // attn: in-place shuffle
-        this.selectedDatasetArray = this.mapppedDatasetArray.slice(0, 5000);
-    
-        this.featuresAndLabel = tf.tensor2d(this.selectedDatasetArray);
-        [this.features, this.label] = tf.split(this.featuresAndLabel, [featureFields.length, 1], 1);
-        //pointsTensor.slice([1,0], [3,4]).print();
-    
-        const {mean, variance} = tf.moments(features, 0); //  The dimension(s) along with to compute
+    #buildFeaturesAndLabel() {
+       
+        const {mean, variance} = tf.moments(this.features, 0); //  The dimension(s) along with to compute
         this.mean = mean;
         this.variance = variance;
         this.standardizedFeatures = features
             .sub(this.mean)
             .div(this.variance.pow(.5));
-
-        return this;
     }
 
-    predict(values) {
+    predict(sampleValues, topK) {
         //const predictionPoint = [47.38, -122.44, 1970]; //, 705000];
-        const predictionPointFeature1d = tf.tensor1d(values);
-        const standardizedPredictionPointFeature1d = predictionPointFeature1d.sub(mean).div(variance.pow(.5));
+        const predictionPointFeature1d = tf.tensor1d(sampleValues);
+        const standardizedPredictionPointFeature1d = predictionPointFeature1d
+            .sub(this.mean)
+            .div(this.variance.pow(.5));
 
         const mse1d = this.standardizedFeatures
             .sub(standardizedPredictionPointFeature1d)
@@ -44,17 +31,72 @@ class KNN
             .pow(.5);
             //.expandDims(1);
 
-        const {values, indices} = tf.topk(mse1d.mul(-1), 3);    
-        values.print();
-        indices.print();
+        const {values, indices} = tf.topk(mse1d.mul(-1), topK);    
+        this.topKValues1d = values;
+        this.topKIndices1d = indices;        
 
-        const topKIndexes = indices.arraySync();
-        const topKIndexesTensor1d = tf.tensor1d(topKIndexes, 'int32');
-        const topKValues = pointsLabels.gather(topKIndexesTensor1d);
+        const topKIndicesArray = this.topKIndices1d.arraySync();
+        const topKIndexes1d = tf.tensor1d(topKIndicesArray, 'int32');
+        const topKValues2d = this.label.gather(topKIndexes1d);
 
-        return topKValues.sum().dataSync()[0] / topKValues.shape[0];
+        return  topKValues2d.sum().dataSync()[0] / topK;
+    }
+}
+
+
+async function buildFeaturesAndLabel() {
+    const datasetCSV = tf.data.csv("https://ijianhuang.github.io/HouseSales/kc_house_data.csv");
+    const featureFields = ["lat", "long"];
+    const labelField = "price";
+    const mapppedDataset = datasetCSV.map(record => { 
+        const row = [];
+        featureFields.forEach(p => row.push(record[p]));
+        row.push(record[labelField]);
+        return row;
+    });
+    const mapppedDatasetArray = await mapppedDataset.toArray();    
+    tf.util.shuffle(mapppedDatasetArray); // attn: in-place shuffle
+    const selectedDatasetArray = mapppedDatasetArray.slice(0, 5000);
+
+    const featuresAndLabel = tf.tensor2d(selectedDatasetArray);
+    const [features, label] = tf.split(featuresAndLabel, [featureFields.length, 1], 1);
+   
+    return [features, label];    
+}
+
+class FeaturesAndLabelsBuilder {
+    constructor(datasetUrl) 
+    {
+        this.datasetUrl = datasetUrl;
+        this.datasetCSV = tf.data.csv(this.datasetUrl);
+
     }
 
-
+    async buildFeaturesAndLabels(featureFields, labelFields) {
+        this.mapppedDataset = this.datasetCSV.map(record => { 
+            const row = [];
+            featureFields.forEach(p => row.push(record[p]));
+            labelFields.forEach(p => row.push(record[p]));            
+            return row;
+        });
+        this.mapppedDatasetArray = await this.mapppedDataset.toArray();    
+        tf.util.shuffle(this.mapppedDatasetArray); // attn: in-place shuffle
+        this.selectedDatasetArray = this.mapppedDatasetArray.slice(0, 5000);
+    
+        this.featuresAndLabel = tf.tensor2d(this.selectedDatasetArray);
+        [this.features, this.labels] = tf.split(this.featuresAndLabel, [featureFields.length, labelFields.length], 1);
+        
+        return [this.features, this.labels];
+    }
 
 }
+
+const featureFields = ["lat", "long"];
+const labelFields = ["price"];
+const builder = new FeaturesAndLabelsBuilder("https://ijianhuang.github.io/HouseSales/kc_house_data.csv");
+
+const [features, labels] = await builder.buildFeaturesAndLabels(featureFields, labelFields);
+
+const knn = new KNN(features, labels);
+const price = knn.predict([47.38, -122.44], 3);
+
